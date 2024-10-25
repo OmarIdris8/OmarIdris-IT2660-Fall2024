ba782789efd5875e0aea5d5ee86efb498e41bf01
Omar Idris <S01082094@acad.tri-c.edu>
2024-10-24 22:41:03 -0400

    lab with questions

--- VSCODE/Lab3.java
+++ VSCODE/Lab3.java
@@ -58,3 +58,22 @@ public class Main {
       return true;
     }
   }
+
+  // 1. According to the book, what is Big O for this algorithm?
+  // We should analyze the match method and isMatched method. The Outer Loop, match, is observed as n - m + 1, (n is length of text and m is length of pattern). 
+  // Inner Loop, isMatched, executes m times for each itereation of the outer loop. Our Big O for this algorithm is O((n - m + 1) * m) = O(n * m)
+
+  // 2. Did your calculations of elapsedTime align with what the book said about Big O for this brute-force string matching algorithm?
+  // Yes they did, brute-force algorithm is applied for finding a substring(pattern) within a larger string (text). 
+  // The algorithm was simplified to O(n*m) with N as the length of text and m as the length of pattern.
+  // The text and pattern increasing should relate directly to O(n*m). As the string becomes longer, the time increases as well, showing the algorithm can be less efficient for large datasets.
+
+  // 3. Describe what you think the purpose of the exercise is?
+  // This exercises purpose was to practice time complexity with a focus on brute-force string matching algorithm.
+  // It also introduced performance measurment of programs we run, which i found interesting. 
+  // We measured elapsed time for best-case and worst-case scenarios, while observing how input size influences execution time.
+
+  // 4. Is this enough to calculate a best- and worst-case scenario?
+  // The best-case is simple and searches for the first character of the text that should match quickest.
+  // The worst-case scenario pattern has an added character so the program is required to search the text all the way through. 
+  // After running the program a couple times I got different answers, so averaging the results of multiple tests could provide a totally accurate data representation.
